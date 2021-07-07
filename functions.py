6e5d3376898ff1d7c06952c80b203905f8769d06
Carolnesso <acng@ic.ufal.br>
2021-07-07 19:18:31 -0300

    Adiciona file de functions e castes; menu atualizado

---
+++ functions.py
@@ -0,0 +1,5 @@
+def manager_menu():
+    action = ""
+    while action != "0":
+        print('\nHello Manager !\nWhich action you wanna do?\n\n1 - Edit employee attributes\n2 - Timecard\n3 - Syndicate\n4 - Sales\n0 - Exit')
+        action = input('\n')
